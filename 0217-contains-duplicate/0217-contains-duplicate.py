b4517171a67bddcfcd0e0a0991b3957115b6462f
juliusjogela <163301754+juliusjogela@users.noreply.github.com>
2025-07-02 13:07:49 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        seen = []
+        for num in nums:
+            if num  in seen:
+                return True
+            else:
+                seen.append(num)
+        return False
