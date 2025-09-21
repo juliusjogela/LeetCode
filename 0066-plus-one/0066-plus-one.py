093117778b30371da55a25b821463f7d6cb730bb
juliusjogela <163301754+juliusjogela@users.noreply.github.com>
2025-09-21 13:17:01 +0100

    Time: 0 ms (100%), Space: 12.5 MB (9.56%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        n = len(digits)
+
+        # iterate backwards
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+
+        # if all were 9s, we need an extra 1 at the front
+        return [1] + digits
