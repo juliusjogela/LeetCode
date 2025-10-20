8c0582a9229965b624fbbdd5113a296c76ee806f
juliusjogela <163301754+juliusjogela@users.noreply.github.com>
2025-10-20 17:28:16 +0100

    Time: 111 ms (67.31%), Space: 20.7 MB (86.66%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -6,19 +6,16 @@ class Solution(object):
         """
         n = len(height)
         i = 0
-        j = n -1
+        j = n-1
         res = 0
-        while i <= j:
-            ##Calculate the area
-            #save that result and compare if maximum
-            #whichever has lower height must move inwards
-
-            area = min(height[i], height[j]) * (j-i)
-            res = max(area, res)
-            if (height[i] > height[j]):
-                j -= 1
+        while i<j:
+            shorter = min(height[i],height[j])
+            area = shorter * (j-i)
+            res = max(res, area)
+            if(height[i] < height[j]):
+                i = i+1
             else:
-                i +=1
-            
-        
+                j = j-1
         return res
+
+
