ebb9afcf1608c80eb24cf057c40ccdaaac39c57e
Julius Jogela <163301754+juliusjogela@users.noreply.github.com>
2025-10-24 17:16:17 +0100

    Time: 35 ms (45.85%), Space: 22.8 MB (19.32%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,22 +6,20 @@ class Solution(object):
         """
         n = len(nums)
         answer = [1] * n
-
+        
         prefix = [1] * n
         product = 1
         for i in range(n):
             prefix[i] = product
             product = nums[i] * product
-        print(prefix)
-
-        postfix = [1] * n
+        
+        postfix = [1] *n
         product = 1
-        for i in range(n-1, -1,-1):
+        for i in range(n-1, -1, -1):
             postfix[i] = product
             product = nums[i] * product
+       
         for i in range(n):
             answer[i] = postfix[i] * prefix[i]
-        print(postfix)
-        print(answer)
-        return answer         
         
+        return answer
