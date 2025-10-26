5bb4b432556f68f6db7260d0d56ec9bd8563b2ab
Julius Jogela <163301754+juliusjogela@users.noreply.github.com>
2025-10-26 16:10:50 +0000

    Time: 82 ms (8.85%), Space: 14.1 MB (95.25%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,5 +1,3 @@
-from collections import Counter
-
 class Solution(object):
     def topKFrequent(self, nums, k):
         """
@@ -7,7 +5,13 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        freq = Counter(nums)  
-        top_k = freq.most_common(k) 
-        elements_only = [item[0] for item in top_k]
-        return elements_only
+        freq = {}
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+        
+        output = []
+        for i in range(k):
+            biggest = max(freq, key = freq.get)
+            output.append(biggest)
+            freq.pop(biggest)
+        return output
