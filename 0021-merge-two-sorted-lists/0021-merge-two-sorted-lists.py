185bdd451769d0ea8e23924248f54ea55304609f
Julius Jogela <163301754+juliusjogela@users.noreply.github.com>
2025-10-26 10:35:45 +0000

    Time: 0 ms (100%), Space: 12.4 MB (97.43%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -10,30 +10,32 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        dummy = ListNode(val = -1)
-        head = dummy
+        if list1 is None:
+            return list2
+        elif list2 is None:
+            return list1
+        
+        if list1.val > list2.val: #list2 is smaller
+            head = list2
+            list2 = list2.next
+        else:
+            head = list1
+            list1 = list1.next
         curr = head
         while list1 != None and list2 != None:
-            if list1.val < list2.val:
-                curr.next = list1
-                list1 = list1.next
-                curr = curr.next
-            else:
+            if list1.val > list2.val: #list2 is smaller
                 curr.next = list2
                 list2 = list2.next
                 curr = curr.next
-            
-        while list1 != None:
+            else:
+                curr.next = list1
+                list1 = list1.next
+                curr = curr.next
+        if list1 is not None:
             curr.next = list1
-            list1 = list1.next
-            curr = curr.next
-        while list2 != None:
+        else:
             curr.next = list2
-            list2 = list2.next
-            curr = curr.next      
-        return head.next
-
-
+        return head
 
 
         
