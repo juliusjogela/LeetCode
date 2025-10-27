86c8421b9f877aad0f7b8db729ea669eb93b87d8
Julius Jogela <163301754+juliusjogela@users.noreply.github.com>
2025-10-27 15:37:58 +0000

    Time: 0 ms (100%), Space: 12.7 MB (16.03%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -11,8 +11,16 @@ class Solution(object):
         :type q: Optional[TreeNode]
         :rtype: bool
         """
-        if not p and not q: return True
-        if not p or not q: return False
-        return (p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+        if not p and not q:
+            return True
+        if p and not q:
+            return False
+        if not p and q:
+            return False
+        if p.val != q.val:
+            return False
+        return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+        
+        
             
         
