a4f57a48b219fdbb60ca2c9add338d9f4cd44d0b
Julius Jogela <163301754+juliusjogela@users.noreply.github.com>
2025-10-28 09:21:17 +0000

    Time: 259 ms (27.61%), Space: 33.9 MB (10.39%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,27 +4,37 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        def dfs(i, j):
-            if (i < 0 or i >= rows or j < 0 or j >= cols or 
-                grid[i][j] == "0" or (i, j) in visited):
+        def dfs(grid, i, j):
+            
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]):
                 return
 
-            visited.add((i, j))
-            dfs(i-1, j)  # up
-            dfs(i+1, j)  # down
-            dfs(i, j-1)  # left
-            dfs(i, j+1)  # right
-        
-        cols = len(grid[0])
+            if grid[i][j] == "0" or (i, j) in visited:
+                return
+
+            visited.add((i,j))
+
+            dfs(grid, i-1, j)
+            dfs(grid, i+1, j)
+            dfs(grid, i, j-1)
+            dfs(grid, i, j+1)
+
+
+
+
+
         rows = len(grid)
+        cols = len(grid[0])
         visited = set()
         islands = 0
+    
         for i in range(rows):
             for j in range(cols):
-                
+
                 if grid[i][j] == "1" and (i, j) not in visited:
                     islands += 1
-                    dfs(i, j)
-
+                    dfs(grid, i, j)
         return islands
+                
+
 
